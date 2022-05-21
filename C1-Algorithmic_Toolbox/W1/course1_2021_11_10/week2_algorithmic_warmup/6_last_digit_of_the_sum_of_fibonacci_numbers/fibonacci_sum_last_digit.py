e59c6d8a047b7b435b4ecc8c1543ccc2d2ff2220
ievazzade <i.evazzade@gmail.com>
2022-05-21 15:56:58 -0500

    c1-w2-asgnmnt1; fibo huge sum last digit

--- C1-Algorithmic_Toolbox/W1/course1_2021_11_10/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ C1-Algorithmic_Toolbox/W1/course1_2021_11_10/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,21 +1,23 @@
 # Uses python3
 import sys
+import math
 
-def fibonacci_sum_naive(n):
+def fibonacci_sum(n):
+    n = n+2
     if n <= 1:
         return n
 
     previous = 0
     current  = 1
-    _sum      = 1
 
     for _ in range(n - 1):
-        previous, current = current, previous + current
-        _sum += current
+        previous, current = current, (previous + current)%10
 
-    return _sum % 10
+
+    return current-1
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(fibonacci_sum_naive(n))
+    input_n = input()
+    n = int(input_n)
+    print(fibonacci_sum(n))
