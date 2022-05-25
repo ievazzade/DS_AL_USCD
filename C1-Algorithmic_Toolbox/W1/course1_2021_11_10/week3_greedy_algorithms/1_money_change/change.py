14c4ea6887b8f1e63bd600cd42b79eb449e3f401
ievazzade <i.evazzade@gmail.com>
2022-05-25 17:35:53 -0500

    change iterative

--- C1-Algorithmic_Toolbox/W1/course1_2021_11_10/week3_greedy_algorithms/1_money_change/change.py
+++ C1-Algorithmic_Toolbox/W1/course1_2021_11_10/week3_greedy_algorithms/1_money_change/change.py
@@ -5,7 +5,18 @@ def get_change(m):
     #write your code here
     m = int(m)
     return m//10 + (m%10)//5 + m%5
+    
+def change_iter(m):
+    m = int(m)
+    if m == 0:
+        return 0
+    num_coin = 0
+    for i in [10,5,1]:
+        num_coin += m//i
+        m = m%i
+    return num_coin
+
 
 if __name__ == '__main__':
     m = input()
-    print(get_change(m))
+    print(change_iter(m))
