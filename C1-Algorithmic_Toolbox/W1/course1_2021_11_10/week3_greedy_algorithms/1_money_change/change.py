ee9286e3a0a3a0e998b9e28f967d5c9c7ced92f2
ievazzade <i.evazzade@gmail.com>
2022-05-25 17:30:52 -0500

    money change one liner

--- C1-Algorithmic_Toolbox/W1/course1_2021_11_10/week3_greedy_algorithms/1_money_change/change.py
+++ C1-Algorithmic_Toolbox/W1/course1_2021_11_10/week3_greedy_algorithms/1_money_change/change.py
@@ -3,8 +3,9 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    m = int(m)
+    return m//10 + (m%10)//5 + m%5
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = input()
     print(get_change(m))
